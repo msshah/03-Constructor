36efa011040099f78581d251017f966e82e1d92e
Mian S. Shah <safyanshah@gmail.com>
2017-08-23 13:14:45 +0500

    Add files via upload
    While any of the available IDEs will work, I prefer you use Command Line for these programs.

---
+++ Constructor.java
@@ -0,0 +1,34 @@
+/**
+	* Written by Mian Safyan Shah
+	* Contact : safyanshah@live.com
+	*
+	* This program demonstrates the use of Constructors in Java and creating Class Object
+	*
+	* Use the command line to compile and run this program. It'll run in any IDE but 
+	* using command line is preferred.
+	*
+	* How to compile : Open Command Prompt on Windows or Terminal on Mac OS/Linux/Unix
+	* then navigate to the directory of this program and type the following command
+	*
+	* javac Constructor.java
+	*
+	* How to Run : after successful compilation, type java Constructor, you'll see output
+	* on the command line.
+	*
+	*
+  */
+  
+  class Constructor
+  {
+  
+  	public Constructor()
+  	{
+  		System.out.println("This is a constructor.");
+  	}
+  	
+  	public static void main(String[] args)
+  	{
+  		Constructor constructor = new Constructor();
+  	}
+  	
+  } // end of class
